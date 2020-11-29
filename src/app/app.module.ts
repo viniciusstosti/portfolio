721e20d3e9e122de6100379fd8d49c225a2a4fd9
Vinicius Santos <vinicius.santos@bliteti.com.br>
2020-11-29 13:49:16 -0300

    Componentizando header e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,14 @@
-import { NgModule, LOCALE_ID } from '@angular/core';
-import { BrowserModule, Title } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { NgModule, LOCALE_ID } from "@angular/core";
+import { BrowserModule, Title } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RouterModule
-  ],
-  providers: [
-    Title,
-    { provide: LOCALE_ID, useValue: 'pt-BR' }
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule],
+  providers: [Title, { provide: LOCALE_ID, useValue: "pt-BR" }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,14 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { HomeComponent } from './home.component';
-import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from "./home.component";
+import { HomeRoutingModule } from "./home-routing.module";
+import { SharedModule } from "src/app/shared/shared.module";
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
-export class HomeModule { }
+export class HomeModule {}
